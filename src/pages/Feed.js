1757d4a996f3d5cbd06fd6db1b99fbf308175507
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2020-04-15 02:08:56 -0300

    .

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -23,7 +23,7 @@ class Feed extends Component {
   }
 
   registerToSocket = () => {
-    const socket = io("http://localhost:3333");
+    const socket = io(process.env.REACT_APP_API_URL);
 
     socket.on("post", (newPost) => {
       this.setState({ feed: [newPost, ...this.state.feed] });
